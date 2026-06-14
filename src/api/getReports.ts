49862ac63e6a3f59a0b14042761bcf34bb8d0688
nassimDev-ctrl <solimannassim51@gmail.com>
2026-06-14 17:38:56 +0300

    feat: enhance getReports API with timeout configuration
    - Added a timeout of 60 seconds to the axios request for improved reliability.
    - Simplified the promise handling by removing unnecessary `.then()` chaining.

--- src/api/getReports.ts
+++ src/api/getReports.ts
@@ -2,15 +2,12 @@ import axios from "axios";
 
 export default function getReports(token: string, start: string, end: string) {
   const apiUrl = `${import.meta.env.VITE_API_URL}categories/aggregates`;
-  return axios
-    .get(apiUrl, {
-      params: { start, end },
-      headers: {
-        "x-api-key": import.meta.env.VITE_API_KEY,
-        "Authorization": `Bearer ${token}`
-      },
-    })
-    .then((res) => {
-      return res;
-    });
+  return axios.get(apiUrl, {
+    params: { start, end },
+    timeout: 60_000,
+    headers: {
+      "x-api-key": import.meta.env.VITE_API_KEY,
+      Authorization: `Bearer ${token}`,
+    },
+  });
 }
